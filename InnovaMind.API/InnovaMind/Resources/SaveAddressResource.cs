d6767ea8daea74ddfd7d7171b53ccbc27534b5d1
Abel Cierto <83315281+AbelCierto@users.noreply.github.com>
2022-11-02 08:12:17 -0500

    feat(resources): Added the class into innovamind/resources

---
+++ InnovaMind.API/InnovaMind/Resources/SaveAddressResource.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InnovaMind.API.InnovaMind.Resources;
+public class SaveAddressResource
+{
+    [Required]
+    [MaxLength(255)]
+    public string address { get; set; }
+
+    [Required]
+    public int userId { get; set; }
+}
